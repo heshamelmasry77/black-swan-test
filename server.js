b43fa4b3038698e5a7dbcfd73759d0e6a09de8aa
heshamelmasry77 <heshamelmasry@outlook.com>
2017-10-11 17:22:08 +0200

    End point to post tasks , mongoose , schema for a task
    1. Using mongoose,
    2. End point for get tasks,
    3. Schema for a task.

--- server.js
+++ server.js
@@ -1,23 +1,46 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb').MongoClient;
+// var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 
-var database;
+// var database;
+
+var Task = mongoose.model('Task', {
+  name: String,
+  description: String,
+  date_time: {type: Date, default: Date.now}
+});
 
 app.use(bodyParser.json());
 
+app.get('/api/task', GetTasks);
+
 app.post('/api/task', function(req, res) {
   console.log(req.body);
-  database.collection('tasks').insertOne(req.body);
+  // database.collection('tasks').insertOne(req.body);
+  var task = new Task(req.body);
+  task.save();
 
   res.status(200);
 });
 
-mongo.connect('mongodb://localhost:27017/blackswan', function(err, db) {
+function GetTasks(res) {
+  Task.find({}).exec(function(err, result) {
+    // console.log(result);
+    if (!err) {
+      res.send(result);
+    } else {
+      console.log('get tasks not available');
+    }
+  });
+}
+
+mongoose.connect('mongodb://localhost:27017/blackswan', function(err) {
   if (!err) {
     console.log('we are connected to mongo');
-    database = db;
+    // database = db;
+    // GetTasks();
   }
 });
 
