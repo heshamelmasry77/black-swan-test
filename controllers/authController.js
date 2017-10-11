3a0809a2675aedce68a2a3010d016e94b73e5358
heshamelmasry77 <heshamelmasry@outlook.com>
2017-10-11 18:41:25 +0200

    Staring authentication by email, adding new field to the user model email to do authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,25 @@
 var User = require('../models/user');
 
 module.exports = {
-  register:  function(req, res) {
+  register: function(req, res) {
     console.log(req.body);
+    User.findOne({email: req.body.email}, function(err, existingUser) {
 
-    var user = new User(req.body);
-    user.save(function(err, result) {
-      if(err){
-        res.status(500).send({
-          message: err.message
+      if (existingUser) {
+        //409 means conflict
+        return res.status(409).send({message: 'email is already registered'});
+      } else {
+        var user = new User(req.body);
+        user.save(function(err, result) {
+          if (err) {
+            res.status(500).send({
+              message: err.message
+            });
+          }
+          res.status(200);
         });
       }
-      res.status(200);
-    })
+    });
   }
 };
 
--- models/user.js
+++ models/user.js
@@ -3,5 +3,6 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('User', {
   username: String,
   first_name: String,
-  last_name: String
+  last_name: String,
+  email: String
 });
