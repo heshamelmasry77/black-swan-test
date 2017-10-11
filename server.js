9a04ae02b9cb4e10e31277811af76cdd67c35aa3
heshamelmasry77 <heshamelmasry@outlook.com>
2017-10-11 16:29:54 +0200

    Adding end point to post tasks and using mongodb

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
+
+var database;
+
+app.use(bodyParser.json());
+
+app.post('/api/task', function(req, res) {
+  console.log(req.body);
+  database.collection('tasks').insertOne(req.body);
+
+  res.status(200);
+});
+
+mongo.connect('mongodb://localhost:27017/blackswan', function(err, db) {
+  if (!err) {
+    console.log('we are connected to mongo');
+    database = db;
+  }
+});
 
 var server = app.listen(5000, function() {
   console.log('listening on port', server.address().port);
